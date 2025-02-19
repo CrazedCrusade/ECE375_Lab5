375426c6cda9da014371fdcae06d8dc385cb633a
Harris Gregory <59817284+CrazedCrusade@users.noreply.github.com>
2025-02-19 15:02:25 -0800

    Add files via upload
    Minor update

--- ECE375_Lab5/ECE375_Lab5/main.asm
+++ ECE375_Lab5/ECE375_Lab5/main.asm
@@ -2,10 +2,33 @@
 ;*	ECE375_lab5.asm
 ;*
 ;*	 Author: Andrew Gondoputro
+;*				and
 ;*			Harrison Gregory
 ;*	   Date: 2/13/2025
 ;*
+;*	Dscrpt: Utilizes code from BumpBot lab #1, but uses 
+;*			interrupts to manage bumpbot behavior instead
+;*			of polling.
 ;*
+/**	Behavior:
+			If only left whisker hit:
+				backup, increment leftWhiskCount, turn right.
+			If Both or right whisker hit:
+				backup, increment rightWhiskCount, turn left.
+				If leftWhisker also hit:
+					also increment leftWhiskCount.
+			If Cleared:
+				set both counters on to zero.
+			Update the Screen to show current counts.
+			Continue forward when nothing else is happening.
+**/
+;*	Additions to lab 1:
+;*			1) displays the number of hits to the
+;*			 left and right sides. 
+;*			2) Includes a clear button to reset number 
+;*			of hits displayed on the screen 
+;*  Pseudocode:
+;*		https://docs.google.com/document/d/1c8lZ_wX4mUPv2ur406kXOY-pYR5Vd4SxY5e5zHvwLnE/edit?usp=sharing
 ;***********************************************************
 
 .include "m32U4def.inc"			; Include definition file
@@ -49,8 +72,16 @@ INIT:							; The initialization routine
 		out		SPH, mpr  ; Store high byte of RAMEND into SPH (stack pointer high byte)
 
 		; Initialize Port B for output
+		ldi		mpr, $FF		; Set Port B Data Direction Register
+		out		DDRB, mpr		; for output
+		ldi		mpr, $00		; Initialize Port B Data Register
+		out		PORTB, mpr		; so all Port B outputs are low
 
 		; Initialize Port D for input
+		ldi		mpr, $00		; Set Port D Data Direction Register
+		out		DDRD, mpr		; for input
+		ldi		mpr, $FF		; Initialize Port D Data Register
+		out		PORTD, mpr		; so all Port D inputs are Tri-State
 
 		; Initialize external interrupts
 			; Set the Interrupt Sense Control to falling edge
@@ -58,6 +89,7 @@ INIT:							; The initialization routine
 		; Configure the External Interrupt Mask
 
 		; Turn on interrupts
+			
 			; NOTE: This must be the last thing to do in the INIT function
 
 ;***********************************************************
@@ -98,10 +130,18 @@ FUNC:							; Begin a function with a label
 ;***********************************************************
 ;*	Stored Program Data
 ;***********************************************************
-
 ; Enter any stored data you might need here
+STRING_BEG:; Address Name for Beginning of string location in flash
+; Each char stored as an Ascii equivalent Int
+.DB		"Right       Left"  ;Upper line	16 char
+STRING_COUNTER_VALS:
+.DB		"000          000"  ;Lower line	16 char
+STRING_END: ; Address Name for end of string location in flash
 
 ;***********************************************************
 ;*	Additional Program Includes
 ;***********************************************************
-; There are no additional file includes for this program
+.include "LCDDriver.asm"		; Include the LCD Driver
+
+;Blank line here to account for Microchip Studio Bug 
+
